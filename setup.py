d95ca9ca5f62b4c663f2294d3e6dbf26f83e93fa
Diego Ciangottini <diego.ciangottini@pg.infn.it>
2023-03-12 22:26:32 +0100

    pipy fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import setuptools
 HERE = Path(__file__).parent.resolve()
 
 # The name of the project
-name = "jupyterlab_custom_kernel"
+name = "jupyterlab-custom-kernel"
 
 lab_path = HERE / name.replace("-", "_") / "labextension"
 
